e71650e43ca149edf31f4679b152ac1ae95ff549
Narquelion <a.kramer103@gmail.com>
2018-04-12 13:13:10 -0500

    Initial commit

---
+++ assignments/hw1/resources/js/hw1.experiment.js
@@ -0,0 +1,89 @@
+/** Construct an instance of the Experiment class.
+  * @constructor
+  * @param {Object} params - The experiment parameters from URL data and/or your data.json file.
+  */
+function Experiment(params) {
+
+  /** Hold the trials, instructions, etc. that make up the experiment.
+   * @type {Array<object>}
+   */
+  var timeline = [];
+
+  /** The current subject.
+   * @type {object}
+   * @param {string} id - The subject's Worker ID or SONA subject number.
+  */
+  var subject = { //NOTE: Add more subject parameters here if needed.
+    id: params.workerId
+  }
+
+  /** Return the subject's ID.
+   * @returns {string} - The subject's Worker ID or SONA subject number.
+  */
+  this.getSubjectId = function() {
+    return subject.id;
+  }
+
+  /** Return the experiment timeline.
+   * @returns {array} - The experiment timeline.
+  */
+  this.getTimeline = function() {
+    return timeline;
+  }
+
+  /** Add data to jsPsych's internal representation of the experiment. Can be called at any time.
+  */
+  this.addPropertiesTojsPsych = function () {
+    jsPsych.data.addProperties({
+      workerId: subject.id
+    });
+  }
+
+  /** Initialize and append the default preamble to the timeline.
+    * This includes a generic intro page, consent form, and demographic questionnaire.
+    * Values for some of these are altered via the JSON file.
+  */
+  var initPreamble = function() {
+    var preamble = params.preamble;
+
+    // NOTE: Functions cannot be included in JSON files - must be appended here instead.
+
+    /* This function checks whether or not the subject consented to the experiment.
+     * jsPsych uses the return value (true/false) to determine whether or not to
+     * display the conditional trial. True -> display the trial. False -> continue
+     * the experiment.
+    */
+    preamble.consent_check.conditional_function = function() {
+      var data = jsPsych.data.getLastTrialData();
+      return !data.consented;
+    }
+
+    // Check that the participant entered a valid age.
+    preamble.demographics_check.conditional_function = function() {
+      var data = jsPsych.data.getLastTrialData();
+      console.log(data);
+      if(parseInt(data.age) < 18) return false;
+      return true;
+    }
+
+    // Add the preamble to the timeline
+    timeline = timeline.concat([preamble.intro, preamble.consent, preamble.consent_check, preamble.demographics, preamble.demographics_check, preamble.post_demographics]);
+  }
+
+  /** This function handles setting up the experimental trials.
+  */
+  var initTrials = function() {
+    var trials = [];
+
+    /* YOUR CODE HERE */
+
+    timeline = timeline.concat(trials);
+  }
+
+  /** Build the experiment.
+  */
+  this.createTimeline = function() {
+    //initPreamble();
+    initTrials();
+  }
+};
